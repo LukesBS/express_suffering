4fc91542d526d55f387bf3d7cc26db6787ae2f11
Luke <luaks.vetter@stud.bs7-augsburg.de>
2022-12-20 12:39:48 +0100

    changed stuff

--- app.js
+++ app.js
@@ -12,6 +12,25 @@ const EXP_PORT = 8080
 // als Middleware die Methode "static" eingesetzt wird
 app.use(express.static('public'));
 
+
+
+//verarbeiten aller GET_Anfragen der Route /meineTestanfrage
+app.get("/meineTestanfrage", (req, res) => {
+    // Senden eines Textes an den Client
+    res.send("Ich habe die Anfrage empfangen");
+});
+
+app.get("/kunde/*", (req, res, next ) => {
+    res.write("Kunde\n");
+    next();
+
+})
+
+app.get("/kunde/anfrage", (req, res) => {
+    res.write("Kunde und Anfrage");
+    res.end();
+})
+
 // Starten des Servers und Übergabe einer Funktion über einen Lambda
 // Ausdruck, welche nach Start ausgeführt werden soll
 app.listen(EXP_PORT, () => {
